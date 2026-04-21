e051c749c3a0b6a553385784bb2e9b47f7efa2fb
linda-aleman <alemanlinda2007@gmail.com>
2026-04-21 17:18:49 -0600

    Implementación de la capa DAL con conexión a SQL Server y CRUD de Producto.

---
+++ DAL/ConexionDB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class ConexionBD
+    {
+        public static string connectionString = "Data Source=LAPTOP-VI0FTHHP;Initial Catalog=IconicFashionDB;Integrated Security=True;Trust Server Certificate=True";
+    }
+}
---
+++ DAL/ProductoDAL.cs
@@ -0,0 +1,54 @@
+﻿using EL;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    public class ProductoDAL
+    {
+        public void InsertarProducto(Producto producto)
+        {
+            using (var context = new IconicFashionContext())
+            {
+                context.Productos.Add(producto);
+                context.SaveChanges();
+            }
+        }
+
+        public List<Producto> ObtenerProductos()
+        {
+            using (var context = new IconicFashionContext())
+            {
+                return context.Productos.ToList();
+            }
+        }
+
+        public void ActualizarProducto(Producto producto)
+        {
+            using (var context = new IconicFashionContext())
+            {
+                context.Productos.Update(producto);
+                context.SaveChanges();
+            }
+        }
+
+        public void EliminarProducto(int idProducto)
+        {
+            using (var context = new IconicFashionContext())
+            {
+                var producto = context.Productos.Find(idProducto);
+                if (producto != null)
+                {
+                    context.Productos.Remove(producto);
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
